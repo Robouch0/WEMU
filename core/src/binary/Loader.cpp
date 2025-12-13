dbc353eaf1396867d05e7f0242dc2ed398b6515d
Hugo <hugo.duchemin@epitech.eu>
2025-12-13 19:14:44 +0900

    fix: merge problem from reorder in Loader.cpp file

--- core/src/binary/Loader.cpp
+++ core/src/binary/Loader.cpp
@@ -46,13 +46,6 @@ void Core::Loader::loadHeader()
     m_bin.header.e_shstrndx = m_beDecoder.extractSwap<Elf32_Half>();
 }
 
-void Core::Loader::loadSections()
-{
-    loadSectionsRaw();
-    loadSectionsName();
-    updateSectionsAddress();
-}
-
 void Core::Loader::loadSectionsRaw()
 {
     m_bin.sections.resize(m_bin.header.e_shnum);
@@ -77,24 +70,6 @@ void Core::Loader::loadSectionsName()
     for (std::size_t i = 0; i < m_bin.header.e_shnum; i++) {
         auto &section = m_bin.sections[i];
         section.name = sectionNames + section.raw.header.sh_name;
-    }
-}
-
-void Core::Loader::updateSectionsAddress()
-{
-    for (std::size_t i = 0; i < m_bin.header.e_shnum; i++) {
-        auto &section = m_bin.sections[i];
-        section.meta.address = section.raw.header.sh_addr;
-        section.meta.size = section.raw.data.size();
-        const auto start = section.meta.address;
-        const auto end = section.meta.address + section.meta.size;
-
-        if (section.raw.header.sh_type == SHT_NOBITS)
-            section.meta.size = section.raw.header.sh_size;
-        if (!(section.raw.header.sh_type != SHT_PROGBITS && section.raw.header.sh_type != SHT_NOBITS))
-            updateAddressRangeProgram(section, start, end);
-        if (section.raw.header.sh_type == SHT_RPL_IMPORTS)
-            updatAddressRangeImports(section, end);
     }
 }
 
@@ -143,22 +118,7 @@ void Core::Loader::loadAndDecompressSectionData(Section &section)
     }
 }
 
-<<<<<<< Updated upstream
-void Core::Loader::updateAddressRangeProgram(const Section &section, const std::size_t start, const unsigned long end)
-=======
-void Core::Loader::loadSectionsName()
-{
-    const Section &shStr = m_bin.sections[m_bin.header.e_shstrndx];
-    char const *sectionNames = shStr.raw.data.data();
-
-    for (std::size_t i = 0; i < m_bin.header.e_shnum; i++) {
-        auto &section = m_bin.sections[i];
-        section.name = sectionNames + section.raw.header.sh_name;
-    }
-}
-
 void Core::Loader::loadAddressRangeProgram(const Section &section, const std::size_t start, const unsigned long end)
->>>>>>> Stashed changes
 {
     if (section.raw.header.sh_flags & SHF_EXECINSTR) {
         if (codeAddressRange.first == 0 || start < codeAddressRange.first)
@@ -184,8 +144,6 @@ void Core::Loader::loadAddressRangeImports(Core::Section &section, const unsigne
     }
 }
 
-<<<<<<< Updated upstream
-=======
 void Core::Loader::loadSectionsMeta()
 {
     for (std::size_t i = 0; i < m_bin.header.e_shnum; i++) {
@@ -205,22 +163,6 @@ void Core::Loader::loadSectionsMeta()
     }
 }
 
-void Core::Loader::loadSectionsRaw()
-{
-    m_bin.sections.resize(m_bin.header.e_shnum);
-    for (size_t i = 0; i < m_bin.header.e_shnum; i++) {
-        auto &section = m_bin.sections[i];
-        m_beDecoder.seek(m_bin.header.e_shoff + i * m_bin.header.e_shentsize);
-        loadSectionHeader(section);
-        m_beDecoder.seek(section.raw.header.sh_offset);
-        if (section.raw.header.sh_flags & SHF_DEFLATED) {
-            loadAndDecompressSectionData(section);
-        } else {
-            loadSectionData(section);
-        }
-    }
-}
-
 void Core::Loader::loadSections()
 {
     loadSectionsRaw();
@@ -236,18 +178,6 @@ void Core::Loader::loadSymbolHeader(Utils::BeDecoder &symDecoder, Core::Symbol &
     symbol.raw.header.st_info = symDecoder.extractSwap<unsigned char>();
     symbol.raw.header.st_other = symDecoder.extractSwap<unsigned char>();
     symbol.raw.header.st_shndx = symDecoder.extractSwap<Elf32_Section>();
-}
-
-void Core::Loader::loadSymbolsName()
-{
-    const Section &symStr = m_bin.findSection(".strtab");
-    char const *symbolNames = symStr.raw.data.data();
-
-    for (auto &symbol : m_bin.symbols) {
-        if (symbol.raw.header.st_name > 0 && symbol.raw.header.st_name < symStr.raw.data.size()) {
-            symbol.name = symbolNames + symbol.raw.header.st_name;
-        }
-    }
 }
 
 void Core::Loader::loadSymbolsMeta()
@@ -285,8 +215,6 @@ void Core::Loader::resolveSymbols()
     }
 }
 
-
->>>>>>> Stashed changes
 void Core::Loader::loadSymbols()
 {
     loadSymbolsRaw();
@@ -319,42 +247,4 @@ void Core::Loader::loadSymbolsName()
             symbol.name = symbolNames + symbol.raw.header.st_name;
         }
     }
-}
-
-void Core::Loader::resolveSymbols()
-{
-    for (auto &symbol : m_bin.symbols) {
-        if (symbol.raw.header.st_shndx >= m_bin.sections.size())
-            continue;
-        auto &section = m_bin.sections[symbol.raw.header.st_shndx];
-        const std::size_t offset = symbol.raw.header.st_value - section.raw.header.sh_addr;
-        symbol.meta.address = section.meta.address + offset;
-        if (!symbol.meta.address)
-            continue;
-        const auto type = ELF32_ST_TYPE(symbol.raw.header.st_info);
-        if (type == STT_FUNC)
-            writeFunctionThunk(symbol, section);
-    }
-}
-
-void Core::Loader::loadSymbolHeader(Utils::BeDecoder &symDecoder, Core::Symbol &symbol)
-{
-    symbol.raw.header.st_name = symDecoder.extractSwap<Elf32_Word>();
-    symbol.raw.header.st_value = symDecoder.extractSwap<Elf32_Addr>();
-    symbol.raw.header.st_size = symDecoder.extractSwap<Elf32_Word>();
-    symbol.raw.header.st_info = symDecoder.extractSwap<unsigned char>();
-    symbol.raw.header.st_other = symDecoder.extractSwap<unsigned char>();
-    symbol.raw.header.st_shndx = symDecoder.extractSwap<Elf32_Section>();
-}
-
-void Core::Loader::writeFunctionThunk(Core::Symbol &symbol, Core::Section &section)
-{
-    EncodedInstruction syscall(0);
-    syscall.opcd = INSTRUCTIONARRAY[InstructionID::E_SC].OPCODE;
-    syscall.bd = symbol.meta.index;
-
-    const std::uint32_t offset = symbol.meta.address - section.meta.address;
-    char *sectionData = section.raw.data.data();
-    EncodedInstruction syscallEndianSwapped = syscall.endianSwap();
-    std::memcpy(sectionData + offset, &syscallEndianSwapped, sizeof(EncodedInstruction));
 }
