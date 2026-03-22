88e03243608eecd528e14f5af8d7f94a7a4efea8
Axel Tacheau <axel.tacheau@epitech.eu>
2026-03-22 19:27:12 +0100

    refactor: overload updateOverflow for generic and add-specific cases, adapt DIVW accordingly

--- core/src/cpu/interpreter/Interpreter.cpp
+++ core/src/cpu/interpreter/Interpreter.cpp
@@ -96,18 +96,23 @@ void Core::Interpreter::updateCR(Core::ConditionRegister::Register &cr, const st
     }
 }
 
-void Core::Interpreter::updateOverflow(const std::int32_t &a, const std::int32_t &b, const std::int32_t &result,
-                                       const EncodedInstruction &instr)
+void Core::Interpreter::updateOverflow(const bool overflow, const EncodedInstruction &instr)
 {
     if (!instr.oe)
         return;
 
+    m_xer.ov = overflow;
+    if (m_xer.ov)
+        m_xer.so = true;
+}
+
+void Core::Interpreter::updateOverflow(const std::int32_t &a, const std::int32_t &b, const std::int32_t &result,
+                                       const EncodedInstruction &instr)
+{
     const bool aSign = a < 0;
     const bool bSign = b < 0;
     const bool resultSign = result < 0;
+    const bool overflow = (aSign == bSign) && (aSign != resultSign);
 
-    m_xer.ov = (aSign == bSign) && (aSign != resultSign);
-
-    if (m_xer.ov)
-        m_xer.so = true;
+    this->updateOverflow(overflow, instr);
 }
