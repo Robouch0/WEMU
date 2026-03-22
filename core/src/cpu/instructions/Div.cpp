266f1c6d68eaf27c0a2ff2e138818d60cc59654d
NicolasMelet <nicolas.melet@epitech.eu>
2026-03-22 12:02:31 +0100

    feat(core): instructions DIVWU / DIVWU. / DIVWUO / DIVWUO.

--- core/src/cpu/instructions/Div.cpp
+++ core/src/cpu/instructions/Div.cpp
@@ -40,4 +40,36 @@ namespace Core::Instruction {
             cpu.m_cr.cr0.eq = result == 0;
         }
     }
+
+    /**
+     * @brief The quotient (RA) / (RB) is placed into register RT.
+     * @param cpu
+     * @param instr
+     */
+    void DIVWU(Core::Interpreter &cpu, const EncodedInstruction &instr)
+    {
+        const std::uint64_t ra = cpu.m_gpr[instr.ra];
+        const std::uint64_t rb = cpu.m_gpr[instr.rb];
+
+        if (instr.oe) {
+            cpu.m_xer.ov = rb == 0;
+
+            if (cpu.m_xer.ov) {
+                cpu.m_xer.so = true;
+                if (instr.rc)
+                    cpu.m_cr.cr0.so = true;
+                return;
+            }
+        }
+
+        const std::uint64_t result = ra / rb;
+        cpu.m_gpr[instr.rt] = static_cast<std::uint32_t>(result);
+
+        if (instr.rc) {
+            const std::int32_t signedRt = cpu.m_gprSigned[instr.rt];
+            cpu.m_cr.cr0.lt = signedRt < 0;
+            cpu.m_cr.cr0.gt = signedRt > 0;
+            cpu.m_cr.cr0.eq = signedRt == 0;
+        }
+    }
 };
