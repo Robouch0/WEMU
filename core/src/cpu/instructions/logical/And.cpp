70841b26fb9732d343db5b21a8fbcf60a22ac4b9
NicolasMelet <nicolas.melet@epitech.eu>
2026-03-23 23:33:39 +0900

    feat(core): instructions NAND / NAND.

--- core/src/cpu/instructions/logical/And.cpp
+++ core/src/cpu/instructions/logical/And.cpp
@@ -30,4 +30,10 @@ namespace Core::Instruction {
         cpu.m_gpr[instr.ra] = cpu.m_gpr[instr.rs] & ~(cpu.m_gpr[instr.rb]);
         cpu.updateCR(cpu.m_cr.cr0, cpu.m_gprSigned[instr.ra], instr, true);
     }
+
+    void NAND(Core::Interpreter &cpu, const EncodedInstruction &instr)
+    {
+        cpu.m_gpr[instr.ra] = ~(cpu.m_gpr[instr.rs] & cpu.m_gpr[instr.rb]);
+        cpu.updateCR(cpu.m_cr.cr0, cpu.m_gprSigned[instr.ra], instr, true);
+    }
 };
