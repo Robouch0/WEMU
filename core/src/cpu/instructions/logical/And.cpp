f1b8b485cf6f6ac5f07fc04876f6045a62a50d58
NicolasMelet <nicolas.melet@epitech.eu>
2026-03-23 22:15:09 +0900

    feat(core): instructions AND / AND.

---
+++ core/src/cpu/instructions/logical/And.cpp
@@ -0,0 +1,15 @@
+//
+// Created by nicolas on 3/22/26.
+//
+
+#include "cpu/interpreter/Interpreter.hpp"
+#include "cpu/types/EncodedInstruction.hpp"
+
+namespace Core::Instruction {
+
+    void AND(Core::Interpreter &cpu, const EncodedInstruction &instr)
+    {
+        cpu.m_gpr[instr.ra] = cpu.m_gpr[instr.rs] & cpu.m_gpr[instr.rb];
+        cpu.updateCR(cpu.m_cr.cr0, cpu.m_gprSigned[instr.ra], instr);
+    }
+}
