fd12f4484e2eefee9f2ede3ddcb8449473b8dbd1
Hugo <hugo.duchemin@epitech.eu>
2026-04-15 18:55:30 +0900

    feat(core): added instruction FMR / FMR.

--- core/src/cpu/instructions/float/FloatMove.cpp
+++ core/src/cpu/instructions/float/FloatMove.cpp
@@ -17,9 +17,11 @@ namespace Core::Instruction {
      * @param cpu   Interpreter state.
      * @param instr Encoded instruction (fields: rt as FRT, rb as FRB, rc).
      */
-    void FMR(Interpreter &cpu, const EncodedInstruction &instr);
-    // {
-    // }
+    void FMR(Interpreter &cpu, const EncodedInstruction &instr)
+    {
+        cpu.m_fpr[instr.frt] = cpu.m_fpr[instr.frb];
+        cpu.updateCR1(instr);
+    }
 
     /**
      * @brief Floating-Point Negate.
