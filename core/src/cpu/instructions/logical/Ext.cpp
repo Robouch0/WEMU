78c879e0214534a325814c308ea72a7478b60676
NicolasMelet <nicolas.melet@epitech.eu>
2026-04-16 11:24:32 +0900

    fix(core): instruction EXTSB bitshift to 7 instead of 8

--- core/src/cpu/instructions/logical/Ext.cpp
+++ core/src/cpu/instructions/logical/Ext.cpp
@@ -10,8 +10,8 @@ namespace Core::Instruction {
 
     void EXTSB(Core::Interpreter &cpu, const EncodedInstruction &instr)
     {
-        const std::uint8_t s = cpu.m_gpr[instr.rs] >> 8 & 1;
-        const std::uint8_t valueRs = cpu.m_gpr[instr.rs];
+        const std::uint8_t s = cpu.m_gpr[instr.rs] >> 7 & 1;
+        const std::uint32_t valueRs = static_cast<std::uint8_t>(cpu.m_gpr[instr.rs]);
         cpu.m_gpr[instr.ra] = s ? 0xFFFFFF00 | valueRs : valueRs;
 
         cpu.updateCR(cpu.m_cr.cr0, cpu.m_gprSigned[instr.ra], instr);
