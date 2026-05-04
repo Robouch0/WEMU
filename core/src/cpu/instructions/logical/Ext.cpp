9deee84cf3e990c3d80b9bbd3a3e5fe6cf1fc462
Hugo <hugo.duchemin@epitech.eu>
2026-05-04 10:07:35 +0900

    fix: get rid of EXTB and EXTH duplicate

--- core/src/cpu/instructions/logical/Ext.cpp
+++ core/src/cpu/instructions/logical/Ext.cpp
@@ -7,23 +7,29 @@
 #include "cpu/types/EncodedInstruction.hpp"
 
 namespace Core::Instruction {
-
-    void EXTSB(Core::Interpreter &cpu, const EncodedInstruction &instr)
-    {
-        const std::uint8_t s = cpu.m_gpr[instr.rs] >> 7 & 1;
-        const std::uint32_t valueRs = static_cast<std::uint8_t>(cpu.m_gpr[instr.rs]);
-        cpu.m_gpr[instr.ra] = s ? 0xFFFFFF00 | valueRs : valueRs;
-
-        cpu.updateCR(cpu.m_cr.cr0, cpu.m_gprSigned[instr.ra], instr);
-    }
-
+    /**
+     * @brief Extend Sign Halfword.
+     *        RA = EXTS(RS[16:31]). Sign-extends the low 16 bits of RS to 32 bits.
+     *        If RC=1, updates CR0.
+     * @param cpu   Interpreter state.
+     * @param instr Encoded instruction (fields: rt as RS, ra as RA dest, rc).
+     */
     void EXTSH(Core::Interpreter &cpu, const EncodedInstruction &instr)
     {
-        const std::uint8_t s = cpu.m_gpr[instr.rs] >> 15 & 1;
-        const std::uint32_t valueRs = static_cast<std::uint16_t>(cpu.m_gpr[instr.rs]);
-        cpu.m_gpr[instr.ra] = s ? 0xFFFF0000 | valueRs : valueRs;
-
-        cpu.updateCR(cpu.m_cr.cr0, cpu.m_gprSigned[instr.ra], instr);
+        cpu.m_gpr[instr.ra] = static_cast<std::uint32_t>(static_cast<std::int16_t>(cpu.m_gpr[instr.rt]));
+        cpu.updateCR0(cpu.m_gprSigned[instr.ra], instr);
     }
 
+    /**
+     * @brief Extend Sign Byte.
+     *        RA = EXTS(RS[24:31]). Sign-extends the low 8 bits of RS to 32 bits.
+     *        If RC=1, updates CR0.
+     * @param cpu   Interpreter state.
+     * @param instr Encoded instruction (fields: rt as RS, ra as RA dest, rc).
+     */
+    void EXTSB(Core::Interpreter &cpu, const EncodedInstruction &instr)
+    {
+        cpu.m_gpr[instr.ra] = static_cast<std::uint32_t>(static_cast<std::int8_t>(cpu.m_gpr[instr.rt]));
+        cpu.updateCR0(cpu.m_gprSigned[instr.ra], instr);
+    }
 }
